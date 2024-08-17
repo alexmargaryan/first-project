cb3adaf6f0f8a84fe45ca9325f262628feebb109
Alex <alexmargaryan18@gmail.com>
2024-08-17 22:49:30 +0400

    add husky pre-commit

--- apps/web-client/src/App.tsx
+++ apps/web-client/src/App.tsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import { add } from '@first-project/math/add';
-import { subtract } from '@first-project/math/subtract';
+import { useState } from "react";
+
+import { add } from "@first-project/math/add";
+import { subtract } from "@first-project/math/subtract";
+
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,21 +13,22 @@ function App() {
   const a = 1;
   const b = 2;
 
+  console.log("Hello from the web-client app!");
   console.log(add(a, b));
   console.log(subtract(a, b));
 
   return (
     <>
       <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className='card'>
+      <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -33,7 +36,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className='read-the-docs'>
+      <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
